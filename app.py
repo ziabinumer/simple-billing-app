91f927f029132d325cf7e5e7aacccc89e491abc1
Zia Gondal <ziabinumer@gmail.com>
2022-04-17 17:32:43 +0500

    session temporary

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOADED"] = True
 
 # configure session
-app.config["SESSION_PERMANENT"] = True 
+app.config["SESSION_PERMANENT"] = False 
 app.config["SESSION_TYPE"] = "filesystem"
 
 Session(app)
