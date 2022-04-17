2f3364014a42dc5a11977b3b7ff4fed6e69d57a2
Zia Gondal <ziabinumer@gmail.com>
2022-04-17 16:31:37 +0500

    improve

--- helpers.py
+++ helpers.py
@@ -27,7 +27,7 @@ def login_required(f):
     """
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if session.get("user") is None:
+        if not session.get("user"):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
